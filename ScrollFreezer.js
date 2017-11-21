d8d49330730712292821bd950f81ae329a234ade
Renato de Leão <renatodeleao@gmail.com>
2017-11-21 16:19:49 +0000

    public methods: freeze, defrost, toggleFreeze, getState

--- ScrollFreezer.js
+++ ScrollFreezer.js
@@ -1,18 +1,9 @@
 /**!
  * Name: ScrollFreezer.js
- * Version: 'v0.0.1'
+ * Version: 'v0.0.2'
  * Author: Renato de Leão | https://github.com/@renatodeleao | @renatodeleao
  * Date: 21 Nov 2017
- *
- * Lock/unlock scroll event on given element
- * Block scroll by keeping track of current scrolled distance
- * and apply specific css to mimic freezing.
- * Why not use: overflow:hidden? because if people have their browsers
- * defaulting to show scrollbars (not overlayed ghost ones like mac/safati)
- * Content will jump to right when overflow is applied. It's ugly
- *
- * Technique original seen at facebook
- * Based on: https://stackoverflow.com/a/45230674/2801012
+ * Repo: https://github.com/renatodeleao/ScrollFreezer
  */
 
 (function(){
@@ -30,28 +21,48 @@
       ["overflowY", "scroll"]
     ];
 
-    this.toggleScrollLock = this.toggleScrollLock.bind(this);
+    this.freeze = this.freeze.bind(this);
+    this.defrost = this.defrost.bind(this);
+    this.toggleFreeze = this.toggleFreeze.bind(this);
   }
 
-  ScrollFreezer.prototype.toggleScrollLock = function toggleScrollLock(){
+  ScrollFreezer.prototype.freeze = function freeze(){
+    var self = this;
+
+    self.curScrollLockPos = getScrollPos().y;
+    self.noScrollCSSRules.forEach(function(styleRule){
+      self._el.style[styleRule[0]] = styleRule[1];
+    });
+    self._el.style.top = -self.curScrollLockPos + "px";
+    self.isFrozen = true;
+  }
+
+  ScrollFreezer.prototype.defrost = function defrost(){
+    var self = this;
+
+    // reset styles
+    self.noScrollCSSRules.forEach(function(styleRule){
+      self._el.style[styleRule[0]] = "";
+    });
+    self._el.style.top = ""; // reset
+    window.scrollTo(0, self.curScrollLockPos); // scroll to saved position
+    self.isFrozen = false
+  }
+
+  ScrollFreezer.prototype.toggleFreeze = function toggleFreeze(){
     var self = this;
 
     if(!self.isFrozen){
-      self.curScrollLockPos = getScrollPos().y;
-
-      self.noScrollCSSRules.forEach(function(styleRule){
-        self._el.style[styleRule[0]] = styleRule[1];
-      });
-      self._el.style.top = -self.curScrollLockPos + "px";
-      self.isFrozen = true;
+      self.freeze();
     } else {
-      // reset styles
-      self.noScrollCSSRules.forEach(function(styleRule){
-        self._el.style[styleRule[0]] = "";
-      });
-      self._el.style.top = ""; // reset
-      window.scrollTo(0, self.curScrollLockPos); // scroll to saved position
-      self.isFrozen = false
+      self.defrost();
+    }
+  }
+
+  ScrollFreezer.prototype.getState = function getState(){
+    return {
+      isFrozen: this.isFrozen,
+      curScrollLockPos: this.curScrollLockPos
     }
   }
 
